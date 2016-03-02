fc71472c6fa7ed058f3eb2b8be0edd071cbe9c03
Ben Hawks <acearchr@gmail.com>
2016-03-01 20:03:41 -0600

    Updated Code

---
+++ week06/inclass1.php
@@ -0,0 +1,14 @@
+<?php
+
+
+echo <<<TESTCODE
+	<table border = "1">
+		<tr style='background-color:yellow'>
+			<td>Hello, World!</td>
+			<td>Hello Indeed!</td>
+		</tr>
+	</table>
+TESTCODE;
+
+
+?>
